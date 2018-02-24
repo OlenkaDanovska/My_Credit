908d2fc5126465a3fa808707d52b2502c96c02e1
Olena <zefir.olena@gmail.com>
2018-02-24 23:04:50 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,114 @@
+$(document).ready(function() {
+ 
+  $('#slider-one').owlCarousel({
+    loop:false,
+    margin:10,
+    nav:true,
+    navText:false,
+    dots:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:4
+        }
+    }
+});
+ 
+});
+
+$(document).ready(function() {
+ 
+  $('#slider-two').owlCarousel({
+    loop:false,
+    margin:10,
+    nav:true,
+    navText:false,
+    dots:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:4
+        }
+    }
+});
+ 
+});
+
+$(document).ready(function() {
+ 
+  $('#slider-three').owlCarousel({
+    loop:false,
+    margin:10,
+    nav:true,
+    navText:false,
+    dots:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:4
+        }
+    }
+});
+ 
+});
+
+$(document).ready(function() {
+ 
+  $('#slider-four').owlCarousel({
+    loop:false,
+    margin:10,
+    nav:true,
+    navText:false,
+    dots:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:4
+        }
+    }
+});
+ 
+});
+
+$(document).ready(function() {
+ 
+  $('#slider-five').owlCarousel({
+    loop:false,
+    margin:10,
+    nav:true,
+    navText:false,
+    dots:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:4
+        }
+    }
+});
+ 
+});
